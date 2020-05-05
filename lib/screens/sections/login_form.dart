1a30839e8c02d221d4eda22ae65186235f561d51
Jason Engelbrecht <47205928+jason-engelbrecht@users.noreply.github.com>
2020-05-04 22:40:44 -0700

    Some refactoring and design improvments

--- lib/screens/sections/login_form.dart
+++ lib/screens/sections/login_form.dart
@@ -7,7 +7,7 @@ class LoginForm extends StatefulWidget {
 }
 
 class LoginFormState extends State {
-  double formPadding = 5.0;
+  double formPadding = 6.0;
   String credentials = '';
   TextEditingController emailController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
@@ -15,66 +15,84 @@ class LoginFormState extends State {
   @override
   Widget build(BuildContext context) {
     TextStyle textStyle = Theme.of(context).textTheme.title;
+    Color primaryColorDark = Theme.of(context).primaryColorDark;
 
     return Form(
         child: Container(
-            padding: EdgeInsets.all(15.0),
+            padding: EdgeInsets.all(20),
             child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
-                  Padding(
-                    padding: EdgeInsets.only(bottom: 10.0),
-                    child: Text('GoalMine',
-                        style: TextStyle(
-                            fontSize: 50.0,
-                          color: Theme.of(context).primaryColorDark
-                        )),
-                  ),
-
-                  Padding(
-                      padding: EdgeInsets.only(top: formPadding, bottom: formPadding),
-                      child: TextField(
-                        controller: emailController,
-                        style: textStyle,
-                        keyboardType: TextInputType.emailAddress,
-                        decoration: InputDecoration(
-                            labelText: 'Email',
-                            labelStyle: textStyle,
-                            border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(5.0))),
-                      )),
-
-                  Padding(
-                      padding: EdgeInsets.only(top: formPadding, bottom: formPadding),
-                      child: TextField(
-                        controller: passwordController,
-                        style: textStyle,
-                        keyboardType: TextInputType.visiblePassword,
-                        decoration: InputDecoration(
-                            labelText: 'Password',
-                            labelStyle: textStyle,
-                            border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(5.0))),
-                      )),
-
-                  Padding(
-                      padding: const EdgeInsets.symmetric(vertical: 16.0),
-                      child: RaisedButton(
-                        color: Theme.of(context).primaryColorDark,
-                        textColor: Theme.of(context).primaryColorLight,
-                        onPressed: () {
-                          setState(() {
-                            credentials = 'Email: ${emailController.text}, '
-                                'Password: ${passwordController.text}';
-                          });
-                        },
-                        child: Text(
-                          'Login',
-                          textScaleFactor: 2.0,
-                        ),
-                      )),
-
+                  header(primaryColorDark),
+                  emailField(textStyle),
+                  passwordField(textStyle),
+                  loginButton(primaryColorDark),
                   Text(credentials)
                 ])));
+  }
+
+  Padding header(Color primaryColorDark) {
+    return Padding(
+      padding: EdgeInsets.only(bottom: 10.0),
+      child: Text('GoalMine',
+          style: TextStyle(
+              fontSize: 50.0,
+              color: primaryColorDark
+          )),
+    );
+  }
+
+  Padding emailField(TextStyle textStyle) {
+    return Padding(
+        padding: EdgeInsets.only(top: formPadding, bottom: formPadding),
+        child: TextField(
+          controller: emailController,
+          style: textStyle,
+          keyboardType: TextInputType.emailAddress,
+          decoration: InputDecoration(
+              labelText: 'Email',
+              labelStyle: textStyle,
+              border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(5.0))),
+        ));
+  }
+
+  Padding passwordField(TextStyle textStyle) {
+    return Padding(
+        padding: EdgeInsets.only(top: formPadding, bottom: formPadding),
+        child: TextField(
+          controller: passwordController,
+          style: textStyle,
+          keyboardType: TextInputType.visiblePassword,
+          decoration: InputDecoration(
+              labelText: 'Password',
+              labelStyle: textStyle,
+              border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(5.0))),
+        ));
+  }
+
+  Container loginButton(Color primaryColorDark) {
+    return Container(
+      height: 60,
+      margin: EdgeInsets.only(top: 15),
+      child: SizedBox.expand(
+          child: RaisedButton(
+            color: primaryColorDark,
+            textColor: Colors.white,
+            onPressed: () {
+              setState(() {
+                credentials = 'Email: ${emailController.text}, '
+                    'Password: ${passwordController.text}';
+              });
+            },
+            child: Text('LOGIN',
+              textScaleFactor: 1.5,
+              style: TextStyle(
+                letterSpacing: 1.5,
+              ),
+            ),
+          )),
+    );
   }
 }
